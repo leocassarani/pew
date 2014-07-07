4d3b1620939d6755ba609901887f7bda3a00ab49
Leo Cassarani <leo.cassarani@me.com>
2014-07-07 17:35:02 +0000

    Fork and exec the given command

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
+func main() {
+	command := os.Args[1:]
+	cmd := exec.Command(command[0], command[1:]...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "pew: %v\n", err)
+		os.Exit(1)
+	}
 }
