b23caa02a2d3428d9d2879bff2dcb8576309479e
Leo Cassarani <leo.cassarani@me.com>
2014-07-07 23:19:07 +0000

    Poll and output the RSS for the child process every second

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -11,13 +16,47 @@ func main() {
 	cmd := exec.Command(command[0], command[1:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+
 	err := cmd.Start()
 	if err != nil {
 		exit(err)
 	}
+
+	go poll(cmd.Process)
+
 	err = cmd.Wait()
 	if err != nil {
 		exit(err)
+	}
+}
+
+func poll(process *os.Process) {
+	filepath := path.Join("/proc", strconv.Itoa(process.Pid), "stat")
+	file, err := os.Open(filepath)
+	if err != nil {
+		exit(err)
+	}
+	defer file.Close()
+
+	for {
+		_, err := file.Seek(0, 0)
+		if err != nil {
+			exit(err)
+		}
+
+		text, err := ioutil.ReadAll(file)
+		if err != nil {
+			exit(err)
+		}
+
+		fields := strings.Split(string(text), " ")
+		rss, err := strconv.Atoi(fields[23])
+		if err != nil {
+			exit(err)
+		}
+		fmt.Println(rss * os.Getpagesize())
+
+		<-time.After(1 * time.Second)
 	}
 }
 
